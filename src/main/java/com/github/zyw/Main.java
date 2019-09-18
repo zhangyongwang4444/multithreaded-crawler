f698275be14e04b36284771df5acea23bc32be7f
zhangyongwang <20225614@qq.com>
2019-09-18 16:27:57 +0800

    算法的初步实现

--- src/main/java/com/github/zyw/Main.java
+++ src/main/java/com/github/zyw/Main.java
@@ -6,19 +6,83 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        CloseableHttpClient httpclient = HttpClients.createDefault();
-        HttpGet httpGet = new HttpGet("https://sina.cn/");
-        try (CloseableHttpResponse response1 = httpclient.execute(httpGet)) {
-            System.out.println(response1.getStatusLine());
-            HttpEntity entity1 = response1.getEntity();
-            // do something useful with the response body
-            // and ensure it is fully consumed
-            System.out.println(EntityUtils.toString(entity1));
+
+        // 待处理的链接池
+        List<String> linkPool = new ArrayList<>();
+        // 已经处理的链接池
+        Set<String> processedLinks = new HashSet<>();
+        linkPool.add("https://sina.cn");
+
+        while (true) {
+            if (linkPool.isEmpty()) {
+                break;
+            }
+            String link = linkPool.remove(linkPool.size() - 1);
+
+            if (processedLinks.contains(link)) {
+                continue;
+            }
+
+            if ((link.contains("news.sina.cn") || "https://sina.cn".equals(link))) {
+                // 这是我们感兴趣的，我们只处理新浪站内的链接
+                CloseableHttpClient httpclient = HttpClients.createDefault();
+
+
+                System.out.println(link);
+                if (link.startsWith("//")) {
+                    link = "https:" + link;
+                    System.out.println(link);
+                }
+
+                HttpGet httpGet = new HttpGet(link);
+                httpGet.addHeader("user-agent", "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Mobile Safari/537.36");
+
+                try (CloseableHttpResponse response1 = httpclient.execute(httpGet)) {
+                    System.out.println(response1.getStatusLine());
+                    HttpEntity entity1 = response1.getEntity();
+
+                    String html = EntityUtils.toString(entity1);
+
+                    Document doc = Jsoup.parse(html);
+
+                    ArrayList<Element> links = doc.select("a");
+
+                    for (Element aTag : links) {
+                        linkPool.add(aTag.attr("href"));
+                    }
+
+                    // 假如这是一个新闻详情页面，就存入数据库，否则，就什么也不做
+                    ArrayList<Element> articleTags = doc.select("article");
+
+                    if (!articleTags.isEmpty()) {
+                        for (Element articleTag : articleTags) {
+                            String title = articleTags.get(0).child(0).text();
+                            System.out.println(title);
+                        }
+                    }
+
+                    processedLinks.add(link);
+
+
+                }
+            } else {
+                // 这是我们不感兴趣的，不处理它
+            }
         }
     }
 }
+
+
